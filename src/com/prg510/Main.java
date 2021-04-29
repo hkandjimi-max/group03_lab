7898c6328d3ced6f31b90d20a58c332bd1207af8
hkadnjimi-max <hermand180@gmail.com>
2021-04-29 10:30:25 +0200

    added a printout statement to main.java

---
+++ src/com/prg510/Main.java
@@ -0,0 +1,10 @@
+package com.prg510;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello git please be good to my code!!");
+        //pull->commit->push
+    }
+}
