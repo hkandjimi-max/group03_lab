91772c554d67598e3a714ead87f2443e959a8fd3
hkandjimi-max <62377463+hkandjimi-max@users.noreply.github.com>
2021-04-29 10:35:00 +0200

    Update Main.java
    I had a qiestoion

--- src/com/prg510/Main.java
+++ src/com/prg510/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello git please be good to my code!!");
         //pull->commit->push
+	   //@john why do you have line xxx
     }
 }
